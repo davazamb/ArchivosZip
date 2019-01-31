a5381ceff1f089c14e30b59cf6a05e53bc2a84b1
davazamb <david.zambrano10@gmail.com>
2019-01-31 10:19:14 -0300

    Creado metodo de accion en el controlador home y la vista

--- ArchivosZip/ArchivosZip/Controllers/HomeController.cs
+++ ArchivosZip/ArchivosZip/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ArchivosZip.Models;
+using Ionic.Zip;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +13,43 @@ namespace ArchivosZip.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/Files/"));
+            List<ArchivoModel> files = new List<ArchivoModel>();
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new ArchivoModel()
+                {
+                    FileName = Path.GetFileName(filePath),
+                    FilePath = filePath
+                });
+            }
+
+            return View(files);
+        }
+
+        [HttpPost]
+        public ActionResult Index(List<ArchivoModel> files)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AlternateEncodingUsage = ZipOption.AsNecessary;
+                zip.AddDirectoryByName("Files");
+                foreach (ArchivoModel file in files)
+                {
+                    if (file.IsSelected)
+                    {
+                        zip.AddFile(file.FilePath, "Files");
+                    }
+                }
+                string zipName = String.Format("Comprimido_{0}.zip", DateTime.Now.ToString("yyyy-MMM-dd-HHmm"));
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    zip.Save(memoryStream);
+                    return File(memoryStream.ToArray(), "application/zip", zipName);
+                }
+            }
+
+
         }
 
         public ActionResult About()
