44ebcadfc02c5d9b7a995c2918f80141bb4ee8e1
davazamb <david.zambrano10@gmail.com>
2019-01-31 10:14:35 -0300

    Creado la clase archivomodel en carpeta models

---
+++ ArchivosZip/ArchivosZip/Models/ArchivoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchivosZip.Models
+{
+    public class ArchivoModel
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
